35b75aae3928af95eedb55b0f103eef79e5fcaf8
Stefan Froemken <froemken@gmail.com>
2018-02-02 15:04:45 +0100

    Switch changeName to DuplicationBehavior

--- Classes/Property/TypeConverter/UploadMultipleFilesConverter.php
+++ Classes/Property/TypeConverter/UploadMultipleFilesConverter.php
@@ -226,7 +226,7 @@ class UploadMultipleFilesConverter extends AbstractTypeConverter
     {
         // upload file
         $uploadFolder = ResourceFactory::getInstance()->retrieveFileOrFolderObject('uploads/tx_clubdirectory/');
-        $uploadedFile = $uploadFolder->addUploadedFile($source, 'changeName');
+        $uploadedFile = $uploadFolder->addUploadedFile($source, \TYPO3\CMS\Core\Resource\DuplicationBehavior::RENAME);
         // create Core FileReference
         return ResourceFactory::getInstance()->createFileReferenceObject(
             [
