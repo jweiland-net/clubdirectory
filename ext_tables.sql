6a3289c6f5a8cf6ff7b23457736432005ae7ebed
Stefan Froemken <froemken@gmail.com>
2018-11-02 11:37:08 +0100

    Use maps2 API instead of creating tx_masp2_uid manually

--- ext_tables.sql
+++ ext_tables.sql
@@ -72,7 +72,6 @@ CREATE TABLE tx_clubdirectory_domain_model_address (
 	telephone varchar(255) DEFAULT '' NOT NULL,
 	fax varchar(255) DEFAULT '' NOT NULL,
 	barrier_free varchar(255) DEFAULT '' NOT NULL,
-	tx_maps2_uid int(11) unsigned DEFAULT '0',
 	club int(11) unsigned DEFAULT '0',
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
