c2c13b6bd9fe30b5670c5c252b98701210b74f59
Stefan Froemken <froemken@gmail.com>
2018-11-20 08:07:39 +0100

    Replace CreateMap with Maps2 Registry

--- Configuration/TCA/Overrides/tx_clubdirectory_domain_model_address.php
+++ Configuration/TCA/Overrides/tx_clubdirectory_domain_model_address.php
@@ -1,6 +1,22 @@
 <?php
-// Update the category registry
-$result = \JWeiland\Maps2\Tca\Maps2Registry::getInstance()->add(
-    'clubdirectory',
-    'tx_clubdirectory_domain_model_address'
-);
+// Add tx_maps2_uid column to address table
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('maps2')) {
+    \JWeiland\Maps2\Tca\Maps2Registry::getInstance()->add(
+        'clubdirectory',
+        'tx_clubdirectory_domain_model_address',
+        [
+            'addressColumns' => ['street', 'house_number', 'zip', 'city'],
+            'defaultCountry' => 'Deutschland',
+            'defaultStoragePid' => [
+                'extKey' => 'clubdirectory',
+                'property' => 'poiCollectionPid'
+            ],
+            'synchronizeColumns' => [
+                [
+                    'foreignColumnName' => 'title',
+                    'poiCollectionColumnName' => 'title'
+                ]
+            ]
+        ]
+    );
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,21 +4,16 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-call_user_func(function($extKey) {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'JWeiland.' . $extKey,
-        'Clubdirectory',
-        [
-            'Club' => 'list, listMyClubs, show, new, create, edit, update, search',
-            'Map' => 'new, create, edit, update'
-        ],
-        // non-cacheable actions
-        [
-            'Club' => 'create, update, search',
-            'Map' => 'create, update'
-        ]
-    );
-
-    // use hook to automatically add a map record to current yellow page
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \JWeiland\Clubdirectory\Tca\CreateMap::class;
-}, $_EXTKEY);
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'JWeiland.clubdirectory',
+    'Clubdirectory',
+    [
+        'Club' => 'list, listMyClubs, show, new, create, edit, update, search',
+        'Map' => 'new, create, edit, update'
+    ],
+    // non-cacheable actions
+    [
+        'Club' => 'create, update, search',
+        'Map' => 'create, update'
+    ]
+);
