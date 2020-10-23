9898639f74d01e3917bb7017c5ddeef57481fbe8
Stefan Froemken <froemken@gmail.com>
2020-10-23 16:43:30 +0200

    Add glossary2 as dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,12 +7,12 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'uploadfolder' => '1',
     'version' => '4.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.20-10.4.99',
-            'maps2' => '8.0.0-0.0.0'
+            'maps2' => '8.0.0-0.0.0',
+            'glossary2' => '4.0.0-0.0.0'
         ],
         'conflicts' => [],
         'suggests' => []
