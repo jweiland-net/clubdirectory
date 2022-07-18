4dae9d157755478a04cdb758860471ba7134156f
Stefan Froemken <froemken@gmail.com>
2022-07-18 14:14:16 +0200

    Repair pageTSConfig for category fields

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,10 @@
 <?php
+
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-call_user_func(function () {
+call_user_func(static function () {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
         'JWeiland.clubdirectory',
         'Clubdirectory',
@@ -31,15 +32,30 @@ call_user_func(function () {
         );
     }
 
-    // add clubdirectory plugin to new element wizard
+    // Add clubdirectory plugin to new element wizard
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:clubdirectory/Configuration/TSconfig/ContentElementWizard.txt">'
     );
 
+    try {
+        $extConf = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class);
+        $rootCategory = (int)$extConf->get('clubdirectory', 'rootCategory');
+        $userGroup = (int)$extConf->get('clubdirectory', 'userGroup');
+
+        // @ToDo: rootUid is deprecated since TYPO3 11. Replace while removing TYPO3 10 compatibility
+        $tsConfig = [
+            'TCEFORM.tt_content.pi_flexform.clubdirectory_clubdirectory.sDEFAULT.settings\.category.PAGE_TSCONFIG_ID = ' . $rootCategory,
+            'TCEFORM.tx_clubdirectory_domain_model_club.categories.config.treeConfig.rootUid = ' . $rootCategory,
+            'TCEFORM.tx_clubdirectory_domain_model_club.fe_users.PAGE_TSCONFIG_ID = ' . $userGroup
+        ];
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(implode(LF, $tsConfig));
+    } catch (\TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException | \TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException $exception) {
+        // do nothing
+    }
+
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['clubdirectoryUpdateSlug']
         = \JWeiland\Clubdirectory\Updater\ClubdirectorySlugUpdater::class;
 
-    /** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher */
     $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
         \TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class
     );
