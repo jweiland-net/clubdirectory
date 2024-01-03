1d96b7c7f444fdc087577bcb0bafcd557f27243b
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-01-03 13:13:44 +0100

    Removed registerModule from ext_tables.php as it is moved to Configurations/Backend/Modules.php file

--- ext_tables.php
+++ ext_tables.php
@@ -10,21 +10,6 @@ call_user_func(static function () {
     );
     $extConf = $extensionConfiguration->get('clubdirectory');
 
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'Clubdirectory',
-        'web',    // Make module a submodule of 'web'
-        'club',    // Submodule key
-        '',    // Position
-        [
-            \JWeiland\Clubdirectory\Controller\ExportModuleController::class => 'index'
-        ],
-        [
-            'access' => 'user,group',
-            'icon' => 'EXT:clubdirectory/Resources/Public/Icons/Extension.svg',
-            'labels' => 'LLL:EXT:clubdirectory/Resources/Private/Language/locallang_export.xlf'
-        ]
-    );
-
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
         'tx_clubdirectory_domain_model_club',
         'EXT:clubdirectory/Resources/Private/Language/locallang_csh_tx_clubdirectory_domain_model_club.xlf'
