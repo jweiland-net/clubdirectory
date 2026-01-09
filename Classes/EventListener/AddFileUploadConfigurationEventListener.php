e137e37776c92ab7787c86aa96e1ed7237f850ad
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-01-09 13:59:30 +0100

    [TASK] Implementation of Logo property file upload using Extbase FileUpload

--- Classes/EventListener/AddFileUploadConfigurationEventListener.php
+++ Classes/EventListener/AddFileUploadConfigurationEventListener.php
@@ -13,6 +13,7 @@ namespace JWeiland\Clubdirectory\EventListener;
 
 use JWeiland\Clubdirectory\Event\InitializeControllerActionEvent;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Mvc\Controller\FileUploadConfiguration;
 use TYPO3\CMS\Extbase\Validation\Validator\FileExtensionValidator;
 use TYPO3\CMS\Extbase\Validation\Validator\MimeTypeValidator;
 
@@ -32,22 +33,49 @@ class AddFileUploadConfigurationEventListener extends AbstractControllerEventLis
         }
 
         $mimeTypeValidator = $this->getMimeTypeValidator();
-        $mimeTypeValidator->setOptions(['allowedMimeTypes' => ['image/jpeg']]);
+        $mimeTypeValidator->setOptions([
+            'allowedMimeTypes' => ['image/jpeg', 'image/png'],
+        ]);
 
         $fileExtensionValidator = $this->getFileExtensionValidator();
         $fileExtensionValidator->setOptions(['allowedFileExtensions' => ['jpg', 'jpeg', 'png']]);
 
-        $fileHandlingServiceConfiguration = $this->arguments->getArgument('myArgument')->getFileHandlingServiceConfiguration();
+        // get TypoScript Configuration from event
+        $settings = $event->getSettings();
+
+        // get and assign upload path for logo from settings
+        $uploadFolder = $settings['new']['uploadFolder'] ?? '';
+        if ($uploadFolder === '') {
+            throw new \InvalidArgumentException(
+                'You have forgotten to set an Upload Folder in TypoScript for clubdirectory',
+                1603808777,
+            );
+        }
+
+        $fileHandlingServiceConfiguration = $event
+            ->getArguments()
+            ->getArgument('club')
+            ->getFileHandlingServiceConfiguration();
+
+        $fileUploadConfiguration = (new FileUploadConfiguration('logo'))
+            ->addValidator($mimeTypeValidator)
+            ->addValidator($fileExtensionValidator)
+            ->setMaxFiles(1)
+            ->setUploadFolder($uploadFolder);
+
+        // Set Required for new action
+        if ($event->getActionName() === 'create') {
+            $fileUploadConfiguration->setRequired();
+        }
+
         $fileHandlingServiceConfiguration->addFileUploadConfiguration(
-            (new FileUploadConfiguration('myPropertyName'))
-                ->setRequired()
-                ->addValidator($mimeTypeValidator)
-                ->addValidator($fileExtensionValidator)
-                ->setMaxFiles(1)
-                ->setUploadFolder('1:/user_upload/files/')
+            $fileUploadConfiguration
         );
 
-        $this->arguments->getArgument('myArgument')->getPropertyMappingConfiguration()->skipProperties('myPropertyName');
+        $event->getArguments()
+            ->getArgument('club')
+            ->getPropertyMappingConfiguration()
+            ->skipProperties('logo');
     }
 
     protected function getMimeTypeValidator(): MimeTypeValidator
